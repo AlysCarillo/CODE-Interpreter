76842617322ea3715259ddfdb41b6311bf3ca603
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-03-26 17:54:32 +0800

    Add visitor function for VariableDeclaration, DataType, and AssignmentStatement

--- CODE-Interpreter/CODE-Interpreter/CodeVisitor.cs
+++ CODE-Interpreter/CODE-Interpreter/CodeVisitor.cs
@@ -2,10 +2,19 @@
 using CODE_Interpreter.Content;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq.Expressions;
+using static Antlr4.Runtime.Atn.SemanticContext;
 
 public class CodeVisitor : CodeBaseVisitor<object>
 {
-    private Dictionary<string, object> symbolTable = new Dictionary<string, object>();
+    private Dictionary<string, object> Variables = new Dictionary<string, object>();
+
+    //public CodeVisitor()
+    //{
+    //    Variables["DISPLAY:"] = new Func<object?[], object?>(Operator.Display);
+    //    //Variable["SCAN:"] = new Func<object?[], object?>(Scan);
+    //}
 
     public override object VisitProgram(CodeParser.ProgramContext context)
     {
@@ -23,19 +32,59 @@ public class CodeVisitor : CodeBaseVisitor<object>
         return base.VisitProgram(context);
     }
 
-    //WA NAKO KASABOT HAHAHAHAHA
-    public override object VisitVariableDeclaration(CodeParser.VariableDeclarationContext context)
+    public override object VisitVariableDeclaration([NotNull] CodeParser.VariableDeclarationContext context)
     {
-        // Get the variable name
+        //extract the variable name
         string variableName = context.IDENTIFIER().GetText();
 
-        // Get the variable value
+        //extract the variable value
         object variableValue = Visit(context.expression());
 
-        // Add the variable to the symbol table
-        symbolTable.Add(variableName, variableValue);
-
+        //update the variable in the symbol table
+        Variables[variableName] = variableValue;
         return base.VisitVariableDeclaration(context);
+    }
+
+    public override object VisitDataType(CodeParser.DataTypeContext context)
+    {
+        if (context.INT_TYPE() != null)
+        {
+            // Handle integer data type
+            return typeof(int);
+        }
+        else if (context.CHAR_TYPE() != null)
+        {
+            // Handle character data type
+            return typeof(char);
+        }
+        else if (context.BOOL_TYPE() != null)
+        {
+            // Handle boolean data type
+            return typeof(bool);
+        }
+        else if (context.FLOAT_TYPE() != null)
+        {
+            // Handle float data type
+            return typeof(float);
+        }
+        else
+        {
+            // Invalid data type
+            throw new ArgumentException("Invalid data type");
+        }
+    }
+
+    public override object VisitAssignmentStatement([NotNull] CodeParser.AssignmentStatementContext context)
+    {
+        //extract the variable name
+        string variableName = context.IDENTIFIER().GetText();
+
+        //extract the variable value
+        object variableValue = Visit(context.expression());
+
+        //update the variable in the symbol table
+        Variables[variableName] = variableValue;
+        return base.VisitAssignmentStatement(context);
     }
 
 }
