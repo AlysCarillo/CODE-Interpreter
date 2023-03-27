aae6a9548951fe4b921ba0c9fcc311db4a4f1f9a
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-03-27 10:12:31 +0800

    Create a Lexer and Parser for the CODE

--- CODE-Interpreter/CODE-Interpreter/Program.cs
+++ CODE-Interpreter/CODE-Interpreter/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using CODE_Interpreter.Content;
+using CODE_Interpreter;
+
+var file = "test.code";
+var fileContents = File.ReadAllText(file);
+
+var inputStream = new AntlrInputStream(fileContents);
+
+// Create a lexer and parser for the code
+var codeLexer = new CodeLexer(inputStream);
+CommonTokenStream commonTokenStream = new CommonTokenStream(codeLexer);
+var codeParser = new CodeParser(commonTokenStream);
+var codeContext = codeParser.program();
+
+// Parse the code and walk the parse tree using the CodeVisitor
+var visitor = new CodeVisitor();
+visitor.VisitProgram(codeContext);
